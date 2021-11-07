02e479d16bf53b0937325125beb7c5d21572a2c8
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2021-11-07 00:32:44 -0600

    create web files

---
+++ web/serverConexion.php
@@ -0,0 +1,20 @@
+<?php
+
+class conexion {
+    const user='';
+    const password='';
+    const database='';
+    const server='localhost';
+
+
+    public function conectardatabase() {
+        $conectar = new mysqli(self::server, self::user, self::password, self::database);
+        if ($conectar->connect_errno) {
+           die("Error en conexion".$conectar->connect_error); 
+        }
+
+
+        return $conectar;
+    }
+}
+?>
