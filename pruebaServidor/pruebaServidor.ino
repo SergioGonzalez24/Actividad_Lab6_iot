fa681b0b00d26802058b93c654c92ef50d39cdf5
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2021-11-08 20:49:01 -0600

    upload files

---
+++ pruebaServidor/pruebaServidor.ino
@@ -0,0 +1,47 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+// Sustituir con datos de red a conectar
+const char* ssid     = "LaRataPelona";
+const char* password = "ibbNfwa9Etppt";
+
+String url="http://192.168.3.42 /actividad6/recibe.php";
+
+String id="tarjeta1";
+int valSensor=0;
+int ledEstado=0;
+
+WiFiClient wifiClient;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("\n");
+
+  WiFi.begin(ssid, password);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  HTTPClient http;
+  http.begin(wifiClient,url);
+  http.addHeader("Content-Type","application/x-www-forum-urlencoded");
+
+  valSensor=random(0,60);
+  ledEstado=random(0,1);
+  String postData="id="+id+"&valor="+String(valSensor)+"&ledo="+String(ledEstado);
+  int httpCode=http.POST(postData);
+
+  String respuesta=http.getString();
+
+  Serial.println(httpCode);
+  Serial.println(respuesta);
+
+  http.end();
+  delay(500);
+  
+  
+}
