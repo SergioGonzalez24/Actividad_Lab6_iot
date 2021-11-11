ddaaff1670583cca4c9d0bbee544d72fba407fc8
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2021-11-11 11:55:59 -0600

    Upload Files

--- pruebaServidor/pruebaServidor.ino
+++ pruebaServidor/pruebaServidor.ino
@@ -5,7 +5,9 @@
 #include <ESP8266HTTPClient.h>
 
 //llenar con url de base de datos local
-String url="";
+String ip="10.16.9.195";
+String url="http://"+ip+"/actividad6/recibe.php";
+
 
 String id="tarjeta1";
 int valorSensor=0;
@@ -19,13 +21,18 @@ void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.println("\n");
+  Serial.println("\n");
   wifiSetup();
   
 }
 
 void loop() {
 
+  http.begin(wifiClient,url);
+  http.addHeader("Content-Type","application/x-www-form-urlencoded");
+  
   int led = random(0,2);
+  //valorSensor = random(0,60);
   digitalWrite(LED_BUILTIN, led);
 
   //Se invierten los valores para encender el led del NODE MCU ESP8266
@@ -37,6 +44,7 @@ void loop() {
 
 //CODIGO SENSORES
 
+
 /*¿Que sensor se desea usar?
 *res = 1 --> para utilizar fotoresistencia
 *res = 2 --> para utilizar el sensor de humedad
@@ -44,25 +52,46 @@ void loop() {
 int res=1;
 
 if(res==1) {
-	valorSensor=valFotores();
+  valorSensor=valFotores();
+  Serial.println("\n");
+  Serial.println("******************************");
+  Serial.println("valor fotoresistencia:\t");
+  Serial.println(valorSensor);
+  Serial.println("******************************");
+  Serial.println("\n");
 }
-else if(res==2) {
-	valorSensor=valHumedad();
-}
-  
-  
-  http.begin(wifiClient,url);
-  http.addHeader("Content-Type","application/x-www-form-urlencoded");
 
+else if(res==2) {
+  //valorSensor=valHumedad();
+  Serial.println("\n");
+  Serial.println("******************************");
+  Serial.println("valor humedad:\t");
+  Serial.println(valorSensor);
+  Serial.println("******************************");
+  Serial.println("\n");
+
+}
+ 
+  
 
   String postData="id="+id+"&valorSensor="+String(valorSensor)+"&ledEstado="+String(ledEstado);
   int httpCode=http.POST(postData);
+  
 
   String respuesta=http.getString();
-
-  //Serial.println(postData);
+  Serial.println("\n");
+  Serial.println("\n");
+  Serial.println("******************************");
+  Serial.println("url:\t");
+  Serial.println(url);
+  Serial.println("******************************");
+  Serial.println("data:\t");
+  Serial.println(postData);
+  Serial.println("******************************");
+  Serial.println("Info:\t");
   Serial.println(httpCode);
   Serial.println(respuesta);
+  Serial.println("******************************");
   http.end();
   delay(5000);  
  
--- pruebaServidor/wifiSetup.h
+++ pruebaServidor/wifiSetup.h
@@ -2,19 +2,53 @@
 
 void wifiSetup() {
   // Sustituir con datos de red a conectar
-  const char* ssid     = "";
+  /*
+   * REDES GUARDADAS
+   * ------------------------
+   * ssid LaRataPelona
+   * password ibbNfwa9Etppt
+   * ------------------------
+   * ssid Socios_CGH
+   * password ""
+   * -----------------------
+   * ssid abcdefg
+   * password 123456789
+   */
+  
+  const char* ssid     = "Socios_CGH";
   const char* password = "";
   byte cont = 0;
   byte max_intentos = 50;
 
-  Serial.begin(115200);
-
   WiFi.begin(ssid, password);
+  Serial.println("\n");
+  
   while (WiFi.status() != WL_CONNECTED and cont < max_intentos)  {
+    cont++;
     digitalWrite(LED_BUILTIN,0);
-    delay(50);
+    delay(500);
     digitalWrite(LED_BUILTIN,1);
-    delay(50);
+    delay(500);
+    Serial.print('-');
 }
+  Serial.println("");
+
+  if (cont < max_intentos) {
+    Serial.print("*************************************************************");
+    Serial.print("Conectado a:\t");
+    Serial.println(WiFi.SSID());
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());
+    Serial.print("MAC address:\t");
+    Serial.println(WiFi.macAddress());
+    Serial.print("*************************************************************");
+
+  }
+
+  else {
+    Serial.print("oooooooooooooooooooooooooooooooooooooooooooooooooooo");
+    Serial.println("ERROR");
+    Serial.print("oooooooooooooooooooooooooooooooooooooooooooooooooooo");
+  }
 
 }
